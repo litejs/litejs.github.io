b36f5daa9c8a825e5f29a9a561654d6368b3a57e
Lauri Rooden <lauri@rooden.ee>
2016-02-19 18:48:52 +0200

    Update testman

--- pub/testman.js
+++ pub/testman.js
@@ -2,8 +2,8 @@
 
 
 /*
-* @version    0.3.0
-* @date       2015-06-15
+* @version    0.3.1
+* @date       2015-06-19
 * @stability  2 - Unstable
 * @author     Lauri Rooden <lauri@rooden.ee>
 * @license    MIT License
@@ -30,7 +30,10 @@
 		bold = red = green = reset = ""
 	}
 
+	describe.result = ""
+
 	function print(str) {
+		describe.result += str + "\n"
 		console.log(str)
 	}
 
@@ -76,10 +79,6 @@
 		}
 
 		return "" + actual == "" + expected
-	}
-
-	function msg(actual, expected, message, operator) {
-		return message || actual + operator + expected
 	}
 
 
@@ -204,22 +203,22 @@
 			return testCase
 		},
 		equal: function(actual, expected, message) {
-			return this.ok(actual == expected, msg(actual, expected, message, "=="))
+			return this.ok(actual == expected, message || actual + "==" + expected)
 		},
 		notEqual: function(actual, expected, message) {
-			return this.ok(actual != expected, msg(actual, expected, message, "!="))
+			return this.ok(actual != expected, message || actual + "!=" + expected)
 		},
 		strictEqual: function(actual, expected, message) {
-			return this.ok(actual === expected, msg(actual, expected, message, "==="))
+			return this.ok(actual === expected, message || actual + "===" + expected)
 		},
 		notStrictEqual: function(actual, expected, message) {
-			return this.ok(actual !== expected, msg(actual, expected, message, "!=="))
+			return this.ok(actual !== expected, message || actual + "!==" + expected)
 		},
 		deepEqual: function(actual, expected, message) {
-			return this.ok(deepEqual(actual, expected), msg(actual, expected, message, "deepEqual"))
+			return this.ok(deepEqual(actual, expected), message || actual + "deepEqual" + expected)
 		},
 		notDeepEqual: function(actual, expected, message) {
-			return this.ok(!deepEqual(actual, expected), msg(actual, expected, message, "notDeepEqual"))
+			return this.ok(!deepEqual(actual, expected), message || actual + "notDeepEqual" + expected)
 		},
 		throws: function(fn, message) {
 			var actual = false
@@ -229,7 +228,7 @@
 			} catch(e) {
 				actual = true
 			}
-			return this.ok(actual, msg(actual, expected, message, "throws"))
+			return this.ok(actual, message || "throws")
 		},
 		plan: function(num) {
 			this.planned = num
@@ -286,4 +285,120 @@
 /*
 * http://sourceforge.net/projects/portableapps/files/
 */
+
+
+
+
+describe.GLOBAL = {}
+
+describe.it.goTo = function(route) {
+	location.href = "#" + route
+	setTimeout(this.wait(), 60)
+	return this
+}
+
+describe.it.waitTill = function(actual, options) {
+	var result
+	, count = 30
+	, resume = this.wait()
+
+	if (options.timeout) {
+		count = 0 | (options.timeout / 50)
+	}
+
+	this.ok(function() {
+		return !!result
+	}, options || "Expected: function returns something")
+	test()
+
+	return this
+
+	function test() {
+		result = actual()
+		if (!result && count--) return setTimeout(test, 50)
+		resume()
+	}
+}
+describe.it.viewOpen = function(actual, options) {
+	return this.waitTill(function() {
+		return View(actual).open
+	}, options || "Expected: View "+actual+" should be open")
+}
+describe.it.waitSelector = function(actual, options) {
+	return this.waitTill(function() {
+		return document.body.find(actual)
+	}, options || "Expected: selector "+actual+" should be in dom")
+}
+describe.it.countSelectors = function(actual, expected, options) {
+	this.waitSelector(actual, options)
+	this.ok(function() {
+		var nodes = document.body.findAll(actual)
+		, count = nodes && nodes.length
+		return count === expected
+	}, options || "Number of matches for " + actual + " should be " + expected)
+	return this
+}
+describe.it.haveText = function(actual, expected, options) {
+	this.waitSelector(actual, options)
+	this.waitTill(function() {
+		var node = document.body.find(actual)
+		, txt = node && node[node.tagName == "INPUT" ? "val" : "txt"]().trim()
+		return txt === expected
+	}, options || actual + " should have text: " + expected)
+	return this
+}
+describe.it.fill = function(actual, expected, options) {
+	this.waitSelector(actual, options)
+	this.ok(function() {
+		var node = document.body.find(actual)
+		, val = node && node.val(expected)
+		return val === expected
+	}, options || actual + " should have value " + expected)
+	return this
+}
+describe.it.click = function(actual, expected, options) {
+	this.waitSelector(actual, options)
+	this.ok(function() {
+		var ev
+		, node = document.body.find(actual)
+		, attr = {
+			pointerX: 0, pointerY: 0, button: 0,
+			ctrlKey: false, altKey: false, shiftKey: false, metaKey: false,
+			bubbles: true, cancelable: true
+		}
+
+		if (node) {
+			if (node.dispatchEvent) {
+				ev = document.createEvent("MouseEvents")
+				ev.initMouseEvent("click", true, true, document.defaultView, attr.button,
+					attr.pointerX, attr.pointerY, attr.pointerX, attr.pointerY,
+					attr.ctrlKey, attr.altKey, attr.shiftKey, attr.metaKey,
+					attr.button, node)
+				node.dispatchEvent(ev)
+			} else if (node.click) {
+				node.click()
+			} else if (node.fireEvent) {
+				node.fireEvent("onclick")
+			} else if (typeof node.onclick == "function") {
+				node.onclick()
+			}
+		}
+		return !!node
+	}, options || actual + " should be clickable")
+	return this
+}
+
+describe.it.collectViewsUsage = function() {
+	var viewsUsage = describe.GLOBAL.viewsUsage = {}
+
+	View.on("show", function(route) {
+		var view = View.views[route]
+
+		do {
+			viewsUsage[route] = viewsUsage[route] || 0
+			viewsUsage[route]++
+		} while (route = (view = view.parent || {}).route)
+	})
+	return this
+}
 

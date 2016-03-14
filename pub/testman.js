1071887cceb4136c1b7e62f8fc80c132c85682e1
Lauri Rooden <lauri@rooden.ee>
2016-03-15 01:51:25 +0200

    Update testman

--- pub/testman.js
+++ pub/testman.js
@@ -2,8 +2,8 @@
 
 
 /*
-* @version    0.3.1
-* @date       2015-06-19
+* @version    0.4.0
+* @date       2016-03-14
 * @stability  2 - Unstable
 * @author     Lauri Rooden <lauri@rooden.ee>
 * @license    MIT License
@@ -49,44 +49,55 @@
 		).toLowerCase()
 	}
 
-	function deepEqual(actual, expected) {
+	function deepEqual(actual, expected, circArr) {
 		if (actual === expected) return true
 
-		var key, len
+		// null == undefined
+		if (actual == null && actual == expected) return true
+
+		var key, keysA, keysB, len
 		, actualType = type(actual)
 
-		if (actualType != type(expected)) return false
-
-		if (actualType == "object") {
-			var keysA = Object.keys(actual)
-			, keysB = Object.keys(expected)
-			len = keysA.length
-			if (len != keysB.length || !deepEqual(keysA.sort(), keysB.sort())) return false
-			for (; len--; ) {
-				key = keysA[len]
-				if (!deepEqual(actual[key], expected[key])) return false
-			}
-			return true
+		if (
+			actualType != type(expected) ||
+			actual.constructor !== expected.constructor ||
+			(actualType == "date" && +actual !== +expected) ||
+			typeof actual != "object"
+		) {
+			return false
 		}
 
+		if (!circArr) {
+			circArr = []
+		}
+
+
+		key = circArr.indexOf(actual)
+		if (key > -1) return circArr[key + 1] === expected
+		circArr.push(actual, expected)
+
+		keysA = Object.keys(actual)
+		len = keysA.length
 		if (actualType == "array" || actualType == "arguments") {
-			len = actual.length
-			if (len != expected.length) return false
-			for (; len--; ) {
-				if (!deepEqual(actual[len], expected[len])) return false
-			}
-			return true
+			if (actual.length !== expected.length) return false
+		} else {
+			keysB = Object.keys(expected)
+			if (len != keysB.length || !deepEqual(keysA.sort(), keysB.sort(), circArr)) return false
 		}
-
-		return "" + actual == "" + expected
+		for (; len--; ) {
+			key = keysA[len]
+			if (!deepEqual(actual[key], expected[key], circArr)) return false
+		}
+		return true
 	}
-
 
 	function AssertionError(message, _stackStart) {
 		this.name = "AssertionError"
 		this.message = message
 		if (Error.captureStackTrace) {
 			Error.captureStackTrace(this, _stackStart || AssertionError)
+		} else {
+			this.stack = this.toString() + "\n" + (new Error()).stack
 		}
 	}
 	AssertionError.prototype = Object.create(Error.prototype)
@@ -187,42 +198,88 @@
 		return testCase
 	}
 
+	function stringify(item, tmp) {
+		if (item && typeof item == "object") {
+			if (item.constructor == Object) {
+				tmp = Object.keys(item).slice(0, 5).map(function(key) {
+					return key + ":" + stringify(item[key])
+				})
+				return "{" + (item.length > 5 ? tmp + ",..." : tmp) + "}"
+			}
+			if (Array.isArray(item)) {
+				tmp = item.slice(0, 5).map(stringify)
+				return "[" + (item.length > 5 ? tmp + ",..." : tmp) + "]"
+			}
+			if (item instanceof Date) {
+				return item.toJSON()
+			}
+			return toString.call(item).slice(8, -1) + "(" + item.toString() + ")"
+		}
+		return JSON.stringify(item)
+	}
+
 	TestCase.prototype = describe.it = describe.assert = {
 		wait: Fn.hold,
-		ok: function(value, message) {
+		ok: function ok(value, message, _stackStart) {
 			var testCase = this
-			, prefix = " #" + (testCase.passedAsserts + testCase.failed.length+1)
 			totalAsserts++
 			testCase.totalAsserts++
 			try {
-				if (typeof value == "function") value = value.call(testCase)
-				if (!value) throw new AssertionError(message)
+				if (typeof value == "function") {
+					value = value.call(testCase)
+				}
+				if (!value) {
+					if (!message) {
+						message = "Should be truthy: " + stringify(value)
+					} else if (Array.isArray(message)) {
+						message = stringify(message[0]) + " " + message[1] + " " + stringify(message[2])
+					}
+					message = message + " #" + (testCase.passedAsserts + testCase.failed.length + 1)
+					throw new AssertionError(message, _stackStart || ok)
+				}
 				passedAsserts++
 				testCase.passedAsserts++
 			} catch(e) {
-				testCase.failed.push(message + prefix + (testCase.options.noStack ? "" : "\n" + e.stack))
+				testCase.failed.push(testCase.options.noStack ? e.message : e.stack)
 			}
 			return testCase
 		},
-		equal: function(actual, expected, message) {
-			return this.ok(actual == expected, message || actual + "==" + expected)
+		notOk: function notOk(value, message) {
+			return this.ok(
+				!value,
+				message || "Should be falsy: " + stringify(value),
+				notOk
+			)
 		},
-		notEqual: function(actual, expected, message) {
-			return this.ok(actual != expected, message || actual + "!=" + expected)
+		equal: function equal(actual, expected, message) {
+			return this.ok(
+				deepEqual(actual, expected),
+				message || [actual, "==", expected],
+				equal
+			)
 		},
-		strictEqual: function(actual, expected, message) {
-			return this.ok(actual === expected, message || actual + "===" + expected)
+		notEqual: function notEqual(actual, expected, message) {
+			return this.ok(
+				!deepEqual(actual, expected),
+				message || [actual, "!=", expected],
+				notEqual
+			)
 		},
-		notStrictEqual: function(actual, expected, message) {
-			return this.ok(actual !== expected, message || actual + "!==" + expected)
+		strictEqual: function strictEqual(actual, expected, message) {
+			return this.ok(
+				actual === expected,
+				message || [actual, "===", expected],
+				strictEqual
+			)
 		},
-		deepEqual: function(actual, expected, message) {
-			return this.ok(deepEqual(actual, expected), message || actual + "deepEqual" + expected)
+		notStrictEqual: function notStrictEqual(actual, expected, message) {
+			return this.ok(
+				actual !== expected,
+				message || [actual, "!==", expected],
+				notStrictEqual
+			)
 		},
-		notDeepEqual: function(actual, expected, message) {
-			return this.ok(!deepEqual(actual, expected), message || actual + "notDeepEqual" + expected)
-		},
-		throws: function(fn, message) {
+		throws: function throws(fn, message) {
 			var actual = false
 			, expected = true
 			try {
@@ -230,7 +287,7 @@
 			} catch(e) {
 				actual = true
 			}
-			return this.ok(actual, message || "throws")
+			return this.ok(actual, message || "throws", throws)
 		},
 		plan: function(num) {
 			this.planned = num
@@ -264,14 +321,25 @@
 			fn.call(this)
 			return this
 		},
-		anyOf: function(a, b) {
-			return this.ok( Array.isArray(b) && b.indexOf(a) != -1, "should be one of '" + b + "', got " + a )
+		anyOf: function anyOf(a, b) {
+			return this.ok(
+				Array.isArray(b) && b.indexOf(a) != -1,
+				"should be one of '" + b + "', got " + a,
+				anyOf
+			)
 		},
-		type: function(thing, expected) {
+		type: function assertType(thing, expected) {
 			var t = type(thing)
-			return this.ok( t === expected, "type should be " + expected + ", got " + t )
+			return this.ok(
+				t === expected,
+				"type should be " + expected + ", got " + t,
+				assertType
+			)
 		}
 	}
+
+	TestCase.prototype.deepEqual = TestCase.prototype.equal
+	TestCase.prototype.notDeepEqual = TestCase.prototype.notEqual
 
 	exports.describe = describe.describe = describe
 
@@ -317,7 +385,7 @@
 
 		this.ok(function() {
 			return !!result
-		}, options || "Expected: function returns something")
+		}, options || "Function returns something")
 		test()
 
 		return this
@@ -331,12 +399,12 @@
 	describe.it.viewOpen = function(actual, options) {
 		return this.waitTill(function() {
 			return View(actual).open
-		}, options || "Expected: View "+actual+" should be open")
+		}, options || "View "+actual+" should be open")
 	}
 	describe.it.waitSelector = function(actual, options) {
 		return this.waitTill(function() {
 			return document.body.find.call(document.documentElement, actual)
-		}, options || "Expected: selector "+actual+" should be in dom")
+		}, options || "Selector "+actual+" should be in dom")
 	}
 	describe.it.countSelectors = function(actual, expected, options) {
 		this.waitSelector(actual, options)
@@ -464,7 +532,8 @@
 	}
 
 	describe.it.assertCssUsage = function(options) {
-		var assert = this.it("should use all css rules")
+		options = options || {}
+		var assert = this.test(options.message || "it should use all css rules")
 
 		var sel
 		, selectors = describe.GLOBAL.selectorsUsage
